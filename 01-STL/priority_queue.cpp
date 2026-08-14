ccbf959070910a40454fea487b68b8bbfcef1339
Parv shah <parvs247@gmail.com>
2026-08-14 21:26:55 +0530

    enhanced comments in priority_queue example for clarity

--- 01-STL/priority_queue.cpp
+++ 01-STL/priority_queue.cpp
@@ -10,9 +10,9 @@ int main() {
     pq.push(20);
     pq.push(30);
 
-    cout << "Top: " << pq.top() << endl;
+    cout << "Top: " << pq.top() << endl; // Should print 50, as it's the largest element
 
-    pq.pop();
+    pq.pop(); // remove largest element (50)
 
     cout << "Top after pop: " << pq.top() << endl;
 
