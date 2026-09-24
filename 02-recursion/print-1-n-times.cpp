85a1c27babf59db49e70777a9787844c42c377c9
Parv shah <parvs247@gmail.com>
2026-09-24 17:28:01 +0530

    add recursive functions to print numbers from 1 to N and N to 1

---
+++ 02-recursion/print-1-n-times.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void print1toN(int i,int n){
+    if(i>n){
+        return;
+    }
+
+     cout<<i<<endl;
+     print1toN(i+1,n);
+    
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    print1toN(1,n);
+
+    return 0;
+}
---
+++ 02-recursion/print-n-1-times.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printNto1(int i, int n){
+    if(i<1){
+        return;
+    }
+    cout<<i<<endl;
+    printNto1(i-1,n);
+}
+
+int main(){
+    
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    printNto1(n,n);
+
+    return 0;
+}
